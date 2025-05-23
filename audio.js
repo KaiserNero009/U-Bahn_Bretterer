ac988978065eff8020c923ddc07e5e8567ffb555
KaiserNero009 <nevin.rohner@outlook.com>
2025-05-23 08:40:17 +0200

    Create audio.js

---
+++ audio.js
@@ -0,0 +1,24 @@
+const audio = {
+  coin: new Audio("sounds/coin.wav"),
+  jump: new Audio("sounds/jump.wav"),
+  crash: new Audio("sounds/crash.wav"),
+  music: new Audio("sounds/music.mp3"),
+
+  play(sound) {
+    if (this[sound]) {
+      this[sound].currentTime = 0;
+      this[sound].play();
+    }
+  },
+
+  startMusic() {
+    this.music.loop = true;
+    this.music.volume = 0.3;
+    this.music.play();
+  },
+
+  stopMusic() {
+    this.music.pause();
+    this.music.currentTime = 0;
+  }
+};
